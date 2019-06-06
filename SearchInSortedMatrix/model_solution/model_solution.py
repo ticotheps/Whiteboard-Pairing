e7804a1f28d97cf6f1ff0e47e0273a625f57a7e1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-06 12:15:41 -0400

    Walks through the 5th test case of the solution using commented code

--- SearchInSortedMatrix/model_solution/model_solution.py
+++ SearchInSortedMatrix/model_solution/model_solution.py
@@ -30,9 +30,9 @@ print(search_sorted_matrix(matrix, 1))    #  should print (0, 0)
 #  6th pass: matrix[0][0] = 1; so return (row, col) => return (0, 0)
 
 print(search_sorted_matrix(matrix, 7))    # should print (0, 2)
-#  1st pass: matrix[0][5] = 999 > 1; so col -= 1 => 4, loop thru same row again
-#  2nd pass: matrix[0][4] = 15 > 1; so col -= 1 => 3, loop thru same row again
-#  3rd pass: matrix[0][3] = 12 > 1; so col -= 1 => 2, loop thru same row again
+#  1st pass: matrix[0][5] = 999 > 7; so col -= 1 => 4, loop thru same row again
+#  2nd pass: matrix[0][4] = 15 > 7; so col -= 1 => 3, loop thru same row again
+#  3rd pass: matrix[0][3] = 12 > 7; so col -= 1 => 2, loop thru same row again
 #  4th pass: matrix[0][2] = 7; so return (row, col) => return (0, 2)
 
 print(search_sorted_matrix(matrix, 999))  # should print (0, 5)
@@ -43,5 +43,12 @@ print(search_sorted_matrix(matrix, 1001)) # should print (1, 5)
 #  2nd pass: matrix[1][5] = 1001; so return (row, col) => return (1, 5)
 
 print(search_sorted_matrix(matrix, 104))  # should print (4, 3)
+#  1st pass: matrix[0][5] = 999 > 104; so col -= 1 => 4, loop thru same row again
+#  2nd pass: matrix[1][4] = 35 < 104; so row += 1 => 1, loop thru the next row
+#  3rd pass: matrix[2][4] = 36 < 104; so row += 1 => 2, loop thru the next row
+#  4th pass: matrix[3][4] = 45 < 104; so row += 1 => 3, loop thru the next row
+#  5th pass: matrix[4][4] = 190 > 104; so col -= 1 => 3, loop thru same row again
+#  6th pass: matrix[4][3] = 104; so return (row, col) => return (4, 3)
+
 print(search_sorted_matrix(matrix, 1010)) # should print (-1, -1)
 
