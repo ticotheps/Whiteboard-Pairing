97a1d4417f270b1c1a1dea2c581b5eac279be699
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-25 12:16:23 -0400

    Goes through Polya's for 'my_solution.py'

---
+++ BalancedBrackets/my_solution.py
@@ -0,0 +1,22 @@
+# Understanding the Problem
+
+  # Objective: to determine whether or not brackets are used properly
+
+  # Expected Input: string of opening/closing brackets
+
+  # Expected Output: True or False
+  
+  # Balanced = 2 of each kind of bracket
+
+# Devising a Plan
+
+  # (1) Create a dictionary and a stack
+  # (2) Loop through the string, one character at a time
+      # (3) If 
+  
+
+
+# Implementing the Plan
+
+
+# Reflecting/Iterating
